fbcd23a124b6712a5e4214c047fefa4ac70b9d82
Edson Flavio de Souza <edson.flavio@ufpr.br>
2024-10-23 15:02:27 -0300

    Dia 2 - CLI - Criação de Tabela

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,4 +1,37 @@
-from .config import settings
+import typer
+from typing import Optional
+from beerlog.core import add_beer_to_database
+from beerlog.core import get_beers_from_database
+from rich.table import Table
+from rich.console import Console
 
-def main():
-    print("Hello from", settings.NAME)
+main = typer.Typer(help="Meu gestor de Cerveja")
+console = Console()
+
+@main.command("add")
+def add(
+    name:   str, 
+    style:  str,
+    flavor: int = typer.Option(...),
+    image:  int = typer.Option(...),
+    cost:   int = typer.Option(...)
+):
+    """Add uma nova cerveja no BD"""
+    if add_beer_to_database(name, style, flavor, image, cost):
+        print("🍻 Adicionada ao banco de dados!!")
+    else:
+        print("\n{no_entry}")
+
+@main.command("list")
+def list_beers(style: Optional[str] = None):
+    """Lista as cervejas no BD"""
+    beers = get_beers_from_database()
+    table = Table(title="Beerlog :beer_mug:")
+    headers = ["id", "name", "style", "rate", "date"]
+    for header in headers:
+        table.add_column(header, style="magenta")
+    for beer in beers:
+        beer.date = beer.date.strftime("%Y-%m-%d")
+        values = [str(getattr(beer, header)) for header in headers]
+        table.add_row(*values)
+    console.print(table)
--- beerlog/database.py
+++ beerlog/database.py
@@ -1,6 +1,16 @@
-from sqlmodel import create_engine
+import warnings
+from sqlalchemy.exc import SAWarning
+from sqlmodel.sql.expression import Select, SelectOfScalar
+warnings.filterwarnings("ignore", category=SAWarning)
+SelectOfScalar.inherit_cache = True
+Select.inherit_cache = True
+from sqlmodel import create_engine, Session
 from beerlog.config import settings
 from beerlog import models
 
 engine = create_engine(settings.database.url)
 models.SQLModel.metadata.create_all(engine)
+
+def get_session():
+    """Retorna uma Sessão de Conexão ao BD"""
+    return Session(engine)
